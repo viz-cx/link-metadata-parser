51aa924ceea31d21a66cc3638b17bbf155f258b4
Vladimir Babin <vovababin@gmail.com>
2020-11-09 14:14:43 +0500

    Add example test

---
+++ test/test.js
@@ -0,0 +1,8 @@
+// https://mochajs.org
+
+var assert = require('assert');
+describe('Test example', function() {
+    it('One equal to one', function() {
+        assert.strictEqual(1, 1);
+    });
+});
